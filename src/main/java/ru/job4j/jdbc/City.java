b69fa99b2a36141ddf9c0cce4e8128af39dde3c7
zenit1925 <kostia-85-85@yandex.ru>
2024-10-02 12:19:07 +0300

    3. Отладчик при работе с Базой Данных [#504953 #515533]

---
+++ src/main/java/ru/job4j/jdbc/City.java
@@ -0,0 +1,69 @@
+package ru.job4j.jdbc;
+
+import java.util.Objects;
+
+public class City {
+
+    private int id;
+    private String name;
+    private int population;
+
+    public City(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public City(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("City{id= %s, name= %s, population= %s}",
+                id, name, population);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        City city = (City) obj;
+        return id == city.id && name.equals(city.name) && population == city.population;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, population);
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
